ebfebd8be3dbf0919efd60e56d043878592c1f28
unknown <rubikonspace@ukr.net>
2023-12-21 13:32:09 +0200

    fix_Joi_schema

--- models/contacts/Contact.js
+++ models/contacts/Contact.js
@@ -2,6 +2,7 @@ import { Schema, model } from "mongoose";
 import { version } from "os";
 import { handleSaveError } from "./hooks.js";
 
+export const phoneType = /^\(\d{3}\) \d{3}-\d{4}/;
 const contactSchema = new Schema(
   {
     name: {
@@ -13,7 +14,7 @@ const contactSchema = new Schema(
     },
     phone: {
       type: String,
-      match: /^\(\d{3}\) \d{3}-\d{4}/,
+      match: phoneType,
     },
     favorite: {
       type: Boolean,
--- schemes/contacts-schemes.js
+++ schemes/contacts-schemes.js
@@ -1,4 +1,5 @@
 import Joi from "joi";
+import { phoneType } from "../models/contacts/Contact.js";
 
 export const contactAddScheme = Joi.object({
   name: Joi.string().required().messages({
@@ -7,13 +8,14 @@ export const contactAddScheme = Joi.object({
   email: Joi.string().required().messages({
     "any.required": `missing required email field`,
   }),
-  phone: Joi.string().required().messages({
+  phone: Joi.string().required().pattern(phoneType).messages({
     "any.required": `missing required phone field`,
   }),
+  favorite: Joi.boolean(),
 });
 
 export const contactUpdScheme = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
-  phone: Joi.string(),
+  phone: Joi.string().pattern(phoneType),
 });
